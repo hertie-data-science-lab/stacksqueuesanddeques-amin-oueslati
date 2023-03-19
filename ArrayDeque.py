8303c1686cc0b14a2010c838836493b7825b0d00
amin-oueslati <amin.m.oueslati@gmail.com>
2023-03-19 16:10:25 +0100

    V 1.0
    Final submission

--- ArrayDeque.py
+++ ArrayDeque.py
@@ -1,16 +1,86 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar  5 18:34:01 2023
+Submitted on 19th March 2022
 
-@author: Hannah
+@author: Oskar Krafft | Paul Sharratt | Fabian Mertz | Amin Oueslati
 """
+
+### Assumptions:
+
+# In line with the book, whenever the queue is full and at max capacity, appending on either side will pop (= overwrite) an entry from the opposite side.
+# 
 
 class Empty(Exception): pass
 
+class ArrayDequeMaxlen(): 
+    
+    DEFAULT_CAPACITY = 10
+        
+    def __init__(self, max_len):
+        self._data = [None] * ArrayDequeMaxlen.DEFAULT_CAPACITY
+        self._size = 0
+        self._front = 0
+        self.max_len = max_len
 
-class ArrayDequeMaxlen(): pass
-        
-                
-        
-        
-   
+    def __len__(self):
+        return self._size
+
+    def is_empty(self):
+        return self._size == 0
+
+    def first(self):
+        if self.is_empty():
+            raise Empty("Queue is empty")
+        first = self._data[self._front] 
+        return first
+
+    def last(self):
+        if self.is_empty():
+            raise Empty("Queue is empty")
+        last = self._data[((self._front + self._size) - 1) % len(self._data)]
+        return last
+
+    def dequeue_first(self):
+        if self.is_empty():
+            raise Empty("Queue is empty")
+        answer = self._data[self._front]
+        self._data[self._front] = [None]
+        self._front = (self._front + 1) % len(self._data)
+        self._size -= 1
+        return answer
+
+    def dequeue_last(self):
+        if self.is_empty():
+            raise Empty("Queue is empty")
+        answer = self.last()
+        self._data[((self._front + self._size) - 1) % len(self._data)] = [None]
+        self._size -= 1
+        return answer
+
+    def enqueue_first(self, e):
+        if self._size == len(self._data):
+            self._resize(min(2 * len(self._data), self.max_len))
+        avail = (self._front - 1) % len(self._data)
+        self._data[avail] = e
+        self._front = (self._front - 1) % len(self._data)
+        if self._size != len(self._data):
+            self._size += 1
+
+    def enqueue_last(self, e):
+        if self._size == len(self._data):
+            self._resize(min(2 * len(self._data), self.max_len))
+        avail = (self._front + self._size) % len(self._data)
+        self._data[avail] = e
+        if self._size == len(self._data):
+            self._front = (self._front + 1) % len(self._data)
+        if self._size != len(self._data):
+            self._size += 1
+
+    def _resize(self, cap):
+        old = self._data
+        self._data = [None] * cap
+        walk = self._front
+        for k in range(self._size):
+            self._data[k] = old[walk]
+            walk = (1 + walk) % len(old)
+        self._front = 0
--- main.py
+++ main.py
@@ -1,37 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar  5 18:34:20 2023
+Submitted on 19th March 2022
 
-@author: Hannah
+@author: Oskar Krafft | Paul Sharratt | Fabian Mertz | Amin Oueslati
 """
 
 from ArrayDeque import ArrayDequeMaxlen
 
-     
-        
+          
 AQM = ArrayDequeMaxlen(20)
 
-
-print('Adding last')
-for i in range(100):
-    AQM.append(i)
-    print (i, AQM._data)
-    
-print('\nDelete 80', AQM.remove(80), AQM._data, AQM._front)
-    
-
-
+#print('Adding last')
+#for i in range(100):
+ #   AQM.enqueue_last(i)
+  #  print (i, AQM._data)
     
 print ('\nAdding first')
-for i in range(20, 10, -1):
-    AQM.appendleft(i)
+# for i in range(20, 10, -1):
+for i in range(0, 50):
+    AQM.enqueue_first(i)
     print (i, AQM._data)
     
-    
 print(AQM._front)
-    
 
-    
 print('\nPerforming the removals')
 while not AQM.is_empty():
-    print ('Remove first', AQM[0], AQM.popleft(), 'Remove last', AQM[-1],  AQM.pop())
+    print ('Remove first', AQM.first(), AQM.dequeue_first(), 'Remove last', AQM.last(),  AQM.dequeue_last())
